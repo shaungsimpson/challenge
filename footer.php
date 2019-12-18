07788389a56d38ec81072fa4eef796960e1deb7d
Shaun Simpson <shaungsimpson@gmail.com>
2019-12-18 14:35:09 +1100

    Styled footer for theme

--- footer.php
+++ footer.php
@@ -15,17 +15,7 @@
 
 	<footer id="colophon" class="site-footer">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'challenge' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'challenge' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'challenge' ), 'challenge', '<a href="http://underscores.me/">Shaun Simpson</a>' );
-				?>
+			<img src="<?php echo get_template_directory_uri(); ?>/assets/img/cpc-grayscale.png">
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
